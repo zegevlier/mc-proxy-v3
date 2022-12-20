d411a761141164c9ded0b82dae5c3b6dc0a98419
zegevlier <29954899+zegevlier@users.noreply.github.com>
2022-12-20 21:55:35 +0100

    Move code around to remove excessive nesting

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -55,82 +55,36 @@ async fn process_socket(socket: TcpStream) -> Result<()> {
                 let bytes_read = buf.as_slice();
                 let mut cursor = Cursor::new(&bytes_read);
 
-                if let Ok(length) = Varint::decode(&mut cursor) {
-                    let length = length.value() as usize;
-                    if (length as usize) > bytes_read.len() {
-                        println!("Length is greater than bytes read");
-                        break;
-                    }
-
-                    let bytes = buf.drain(..(length + varint_size(length as i32).unwrap() as usize));
-                    let mut cursor = Cursor::new(&bytes.as_slice()[bytes.len() - length..]);
-                    let id = Varint::decode(&mut cursor).unwrap().value();
-
-                    let conn_info_l = conn_info.lock().await;
-                    let packet =
-                        match decode_packet(conn_info_l.state, Direction::Serverbound, id, &mut cursor)
-                        {
-                            Ok(packet) => packet,
-                            Err(e) => {
-                                println!("Error decoding packet: {}", e);
-                                continue;
-                            }
-                        };
-                    dbg!(&packet);
-                    drop(conn_info_l);
-                    match packet {
-                        Packets::Handshaking(handshaking_packet) => match handshaking_packet {
-                            HandshakingPacket::Serverbound(serverbound_packet) => {
-                                match serverbound_packet {
-                                    ServerboundHandshakingPacket::Handshake(handshake) => {
-                                        let mut conn_info = conn_info.lock().await;
-                                        conn_info.protocol_version =
-                                            Version::from_id(handshake.protocol_version.into());
-                                        conn_info.state = match handshake.next_state {
-                                            handshaking::serverbound::handshake::State::Status => {
-                                                State::Status
-                                            }
-                                            handshaking::serverbound::handshake::State::Login => {
-                                                State::Login
-                                            }
-                                        };
-                                    }
-                                }
-                            }
-                            _ => panic!("Unexpected packet"),
-                        },
-                        Packets::Status(status_packet) => match status_packet {
-                            StatusPacket::Serverbound(serverbound_status_packet) => {
-                                match serverbound_status_packet {
-                                    ServerboundStatusPacket::StatusRequest(_packet) => {
-                                        let conn_info = conn_info.lock().await;
-                                        let status_response_packet = StatusResponse {
-                                            json_response: format!("{{\"version\":{{\"name\":\"1.19.2\",\"protocol\":{}}},\"players\":{{\"max\":1,\"online\":0,\"sample\":[]}},\"description\":{{\"text\":\"Proxy\"}}}}",
-                                                                    conn_info.protocol_version.map_or(-1, |version| version.to_id().unwrap())),
-                                        };
-                                        c_tx.send(Packets::Status(StatusPacket::Clientbound(
-                                            mc_networking::packets::status::ClientboundStatusPacket::StatusResponse(
-                                                status_response_packet,
-                                            ),
-                                        ))).await.unwrap();
-                                    },
-                                    ServerboundStatusPacket::PingRequest(packet) => {
-                                        c_tx.send(Packets::Status(StatusPacket::Clientbound(
-                                            mc_networking::packets::status::ClientboundStatusPacket::PingResponse(
-                                                PingResponse {
-                                                    payload: packet.payload,
-                                                }
-                                            ),
-                                        ))).await.unwrap();
-                                    },
-                                }
-                            }
-                            _ => panic!("Unexpected packet"),
-                        },
-                    }
-                } else {
+                let length = Varint::decode(&mut cursor);
+                if length.is_err() {
+                    println!("Error decoding length");
                     break;
                 }
+
+                let length = length.unwrap().value() as usize;
+
+                if (length as usize) > bytes_read.len() {
+                    println!("Length is greater than bytes read");
+                    break;
+                }
+
+                let bytes = buf.drain(..(length + varint_size(length as i32).unwrap() as usize));
+                let mut cursor = Cursor::new(&bytes.as_slice()[bytes.len() - length..]);
+                let id = Varint::decode(&mut cursor).unwrap().value();
+
+                let conn_info_l = conn_info.lock().await;
+                let packet =
+                    match decode_packet(conn_info_l.state, Direction::Serverbound, id, &mut cursor)
+                    {
+                        Ok(packet) => packet,
+                        Err(e) => {
+                            println!("Error decoding packet: {}", e);
+                            continue;
+                        }
+                    };
+                dbg!(&packet);
+                drop(conn_info_l);
+                handle_packet(packet, &conn_info, &c_tx).await;
             }
         }
     }));
@@ -157,6 +111,55 @@ async fn process_socket(socket: TcpStream) -> Result<()> {
 
     println!("Connection closed");
     Ok(())
+}
+
+async fn handle_packet(packet: Packets, conn_info: &Arc<Mutex<ConnInfo>>, c_tx: &Sender<Packets>) {
+    match packet {
+        Packets::Handshaking(handshaking_packet) => match handshaking_packet {
+            HandshakingPacket::Serverbound(serverbound_packet) => match serverbound_packet {
+                ServerboundHandshakingPacket::Handshake(handshake) => {
+                    let mut conn_info = conn_info.lock().await;
+                    conn_info.protocol_version =
+                        Version::from_id(handshake.protocol_version.into());
+                    conn_info.state = match handshake.next_state {
+                        handshaking::serverbound::handshake::State::Status => State::Status,
+                        handshaking::serverbound::handshake::State::Login => State::Login,
+                    };
+                }
+            },
+            _ => panic!("Unexpected packet"),
+        },
+        Packets::Status(status_packet) => {
+            match status_packet {
+                StatusPacket::Serverbound(serverbound_status_packet) => {
+                    match serverbound_status_packet {
+                        ServerboundStatusPacket::StatusRequest(_packet) => {
+                            let conn_info = conn_info.lock().await;
+                            let status_response_packet = StatusResponse {
+                            json_response: format!("{{\"version\":{{\"name\":\"1.19.2\",\"protocol\":{}}},\"players\":{{\"max\":1,\"online\":0,\"sample\":[]}},\"description\":{{\"text\":\"Proxy\"}}}}",
+                                                    conn_info.protocol_version.map_or(-1, |version| version.to_id().unwrap())),
+                        };
+                            c_tx.send(Packets::Status(StatusPacket::Clientbound(
+                            mc_networking::packets::status::ClientboundStatusPacket::StatusResponse(
+                                status_response_packet,
+                            ),
+                        ))).await.unwrap();
+                        }
+                        ServerboundStatusPacket::PingRequest(packet) => {
+                            c_tx.send(Packets::Status(StatusPacket::Clientbound(
+                            mc_networking::packets::status::ClientboundStatusPacket::PingResponse(
+                                PingResponse {
+                                    payload: packet.payload,
+                                }
+                            ),
+                        ))).await.unwrap();
+                        }
+                    }
+                }
+                _ => panic!("Unexpected packet"),
+            }
+        }
+    }
 }
 
 #[tokio::main]
